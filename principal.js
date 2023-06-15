a905dd519791a09b99cb46a699e11bab06247e06
Fernanda López <fer.daye.2007@gmail.com>
2023-06-15 14:21:43 -0600

    nombres

---
+++ principal.js
@@ -0,0 +1,6 @@
+var cuentasDePersonas=[
+    { id: 1, nombre:"Mai", saldo:"200", pin:"123"},
+    { id: 2, nombre: "Gera", saldo:"290", pin:"231"},
+    { id: 3, nombre: "Maui", saldo:"67", pi:"321"}
+]
+console.log(cuentasDePersonas);
