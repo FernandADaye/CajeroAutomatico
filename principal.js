308c19ea2136ae028f5443c95aaad833c9ac7a24
Fernanda López <fer.daye.2007@gmail.com>
2023-06-19 16:06:56 -0600

    boton de ingreso y pin

--- principal.js
+++ principal.js
@@ -3,4 +3,20 @@ var cuentasDePersonas=[
     { id: 2, nombre: "Gera", saldo:"290", pin:"231"},
     { id: 3, nombre: "Maui", saldo:"67", pi:"321"}
 ]
-console.log(cuentasDePersonas);
+var formulario = document.querySelector('form')
+formulario.addEventListener('submit', (event)=> {
+    let pin = prompt ("Ingrese su pin");
+    if (pin != null){
+        if (pin === '000'){
+            console.log("Correcto");
+        }else{
+            console.log("Incorrecto");
+        }
+    }
+    event.preventDefault()
+    console.log(event.target[0].value);
+})
+
+
+
+
